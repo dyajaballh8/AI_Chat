e4776d7951bbd269ac3efcdccac99e5206269771
dyajaballh8 <dyajaballh8@gmail.com>
2026-08-18 17:53:23 +0300

    Update DAHL_API_KEY in chat.js

--- js/chat.js
+++ js/chat.js
@@ -3,7 +3,7 @@ if (!currentUser) {
   window.location.href = 'signin.html';
 }
 
-const DAHL_API_KEY = 'AQ.Ab8RN6JmULb8xq0kz8uPD6ozZTmN9-GC4VDMjh7Vu_v9ap-FCg';
+const DAHL_API_KEY = 'AQ.Ab8RN6JPdeUr3vHw5vUgoQKWxth__25YkJEZAS8YOwhc7_VoPg';
 const DAHL_URL = 'https://generativelanguage.googleapis.com/v1beta/openai/chat/completions';
 
 const AVAILABLE_MODELS = [
